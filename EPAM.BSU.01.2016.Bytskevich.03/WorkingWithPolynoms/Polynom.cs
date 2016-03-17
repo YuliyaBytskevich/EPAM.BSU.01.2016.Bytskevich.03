152d1f379e4c848fa7ad4278a2abaf7e42486808
Yuliya Bytskevich <yuliya.bytskevich@gmail.com>
2016-03-17 12:21:48 +0300

    Polynom class created, unit tests added (NUnit)

---
+++ EPAM.BSU.01.2016.Bytskevich.03/WorkingWithPolynoms/Polynom.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithPolynoms
+{
+    public class Polynom
+    {
+        public  double[] Coefficients { get; }
+
+        public Polynom(params double[] coefficients)
+        {
+            if (coefficients.Length > 0)
+            {
+                this.Coefficients = new double[coefficients.Length];
+                coefficients.CopyTo(this.Coefficients, 0);
+            }
+            else 
+                throw new ArgumentException();
+        }
+
+        public double CalculateValue(double argument)
+        {
+            double result = 0;
+            for (int i = 0; i < Coefficients.Length; i++)
+                result += Coefficients[i]*Math.Pow(argument, i);
+            return result;
+        }
+
+        public static Polynom operator +(Polynom lhs, Polynom rhs)
+        {
+            double[] longerArray = null, shorterArray = null;
+            if (lhs.Coefficients.Length > rhs.Coefficients.Length)
+            {
+                longerArray = lhs.Coefficients;
+                shorterArray = rhs.Coefficients;
+            }
+            else
+            {
+                longerArray = rhs.Coefficients;
+                shorterArray = lhs.Coefficients;
+            }
+            double[] actualCoefficients = new double[longerArray.Length];
+            longerArray.CopyTo(actualCoefficients, 0);
+            for (int i = 0; i < shorterArray.Length; i++)
+                checked
+                {
+                    actualCoefficients[i] = longerArray[i] + shorterArray[i];
+                }
+            return new Polynom(actualCoefficients);
+        }
+
+        public static Polynom operator -(Polynom lhs, Polynom rhs)
+        {
+            double[] longerArray = null, shorterArray = null;
+            if (lhs.Coefficients.Length > rhs.Coefficients.Length)
+            {
+                longerArray = lhs.Coefficients;
+                shorterArray = rhs.Coefficients;
+            }
+            else
+            {
+                longerArray = rhs.Coefficients;
+                shorterArray = lhs.Coefficients;
+            }
+            double[] actualCoefficients = new double[longerArray.Length];
+            longerArray.CopyTo(actualCoefficients, 0);
+            for (int i = 0; i < shorterArray.Length; i++)
+                actualCoefficients[i] = longerArray[i] - shorterArray[i];
+            if(longerArray != lhs.Coefficients)
+                for (int i = 0; i < actualCoefficients.Length; i++)
+                    checked
+                    {
+                        actualCoefficients[i] = -actualCoefficients[i];
+                    }
+            return new Polynom(actualCoefficients);
+        }
+
+        public static Polynom operator *(Polynom lhs, Polynom rhs)
+        {
+            double[] actualCoefficients = new double[lhs.Coefficients.Length + rhs.Coefficients.Length - 1];
+            for (int i = 0; i < lhs.Coefficients.Length; i++)
+            {
+                for (int j = 0; j < rhs.Coefficients.Length; j++)
+                    actualCoefficients[i + j] += lhs.Coefficients[i]*rhs.Coefficients[j]; object o = new object(); 
+            }           
+            return new Polynom(actualCoefficients);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (obj.GetType() != this.GetType())
+                return false;
+            else if (this.Coefficients.Length != (obj as Polynom).Coefficients.Length)
+                return false;
+            else
+                return this.Coefficients.SequenceEqual((obj as Polynom).Coefficients);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            if (this.Coefficients[0] != 0)
+                result += this.Coefficients[0].ToString() + " ";
+            if (this.Coefficients[1] != 0)
+            {
+                if (this.Coefficients[1] > 0 && result != "")
+                    result += "+";
+                result += this.Coefficients[1].ToString() + "X ";
+            }
+            for (int i = 2; i < this.Coefficients.Length; i++)
+            {
+                if (this.Coefficients[i] != 0)
+                {
+                    if (this.Coefficients[i] > 0 && result != "")
+                        result += "+";
+                    result += this.Coefficients[i].ToString() + "(X^" + i.ToString() + ")";
+                    if (i != this.Coefficients.Length - 1)
+                        result += " ";
+                }
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)(this.Coefficients.Min()*100 - this.Coefficients.Sum()*10 + this.Coefficients.Max() - this.Coefficients.Length * 1000);
+        }
+    }
+}
---
+++ EPAM.BSU.01.2016.Bytskevich.03/WorkingWithPolynomsNUnitTests/PolynomClassTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WorkingWithPolynoms;
+
+namespace WorkingWithPolynomsNUnitTests
+{
+    [TestFixture]
+    public class PolynomClassTests
+    {
+        #region Test method:  public double CalculateValue(double argument)
+        public IEnumerable<TestCaseData> TestCasesForCalculateValue
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynom(1, 1, 2, 3), 10).Returns(3211);
+                yield return new TestCaseData(new Polynom(200, 1, -2, 3), 10).Returns(3010);
+                yield return new TestCaseData(new Polynom(-1, -2, -3, -4), 1).Returns(-10);
+                yield return new TestCaseData(new Polynom(0, 0, 0, 0, 0, 0), 10).Returns(0);
+                yield return new TestCaseData(new Polynom(0, long.MaxValue, long.MaxValue), 1).Returns(2*Convert.ToDecimal(long.MaxValue));
+                yield return new TestCaseData(new Polynom(0, double.MaxValue, double.MaxValue, double.MaxValue), 10).Returns(double.PositiveInfinity);
+                yield return new TestCaseData(new Polynom(0, double.MinValue, double.MinValue, double.MinValue), 10).Returns(double.NegativeInfinity);
+                yield return new TestCaseData(new Polynom(0.1, 0, 0, 0.3), 10).Returns(300.1);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForCalculateValue))]
+        public double CalculateValue_ForArgument(Polynom polynom, double argument)
+        {
+            return polynom.CalculateValue(argument);
+        }
+        #endregion
+
+        #region Test method: public static Polynom operator +(Polynom lhs, Polynom rhs)
+        public IEnumerable<TestCaseData> TestCasesForAddition
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, 0, 0, 0, 4, 5)).Returns("1X +2(X^2) +3(X^3) +4(X^4) +5(X^5)");
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, -1, -2, -3)).Returns("");
+                yield return new TestCaseData(new Polynom(0, 0), new Polynom(0, -1, -2, -3)).Returns("-1X -2(X^2) -3(X^3)");
+                yield return new TestCaseData(new Polynom(1, 2, 3, 4, 5, 6, 7), new Polynom(-10, 0, -10, 0, -10)).Returns("-9 +2X -7(X^2) +4(X^3) -5(X^4) +6(X^5) +7(X^6)");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForAddition))]
+        public string SumTwoPolynoms_ToString(Polynom first, Polynom second)
+        {
+            return (first + second).ToString();
+        }
+        #endregion
+
+        #region Test method: public static Polynom operator -(Polynom lhs, Polynom rhs)
+        public IEnumerable<TestCaseData> TestCasesForSubtraction
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, 0, 0, 0, 4, 5)).Returns("1X +2(X^2) +3(X^3) -4(X^4) -5(X^5)");
+                yield return new TestCaseData(new Polynom(0.1, 0, 0, 0, 0.04, -0.05), new Polynom(0.05, 0, 0, 0, 4, 5)).Returns("0,05 -3,96(X^4) -5,05(X^5)");
+                yield return new TestCaseData(new Polynom(0, 0), new Polynom(0, 0, 0, 0, 0)).Returns("");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForSubtraction))]
+        public string SubtractTwoPolynoms_ToString(Polynom first, Polynom second)
+        {
+            return (first - second).ToString();
+        }
+        #endregion
+
+        #region Test method: public static Polynom operator *(Polynom lhs, Polynom rhs)
+        [Test, TestCaseSource(nameof(TestCasesForMultiplication))]
+        public string MultiplyTwoPolynoms_ToString(Polynom first, Polynom second)
+        {
+            return (first * second).ToString();
+        }
+
+        public IEnumerable<TestCaseData> TestCasesForMultiplication
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, 0, 0, 0, 4, 5)).Returns("4(X^5) +13(X^6) +22(X^7) +15(X^8)");
+                yield return new TestCaseData(new Polynom(1, 1, 0, 2), new Polynom(1, 1, 3)).Returns("1 +2X +4(X^2) +5(X^3) +2(X^4) +6(X^5)");
+                yield return new TestCaseData(new Polynom(0, 0), new Polynom(0, 0, 0, 0, 0)).Returns("");
+            }
+        }
+        #endregion
+
+        #region Test method:  public override bool Equals(object obj)
+        [Test, TestCaseSource(nameof(TestCasesForEquals))]
+        public string ComparingTwoPolynoms_ToString(Polynom first, Polynom second)
+        {
+            return first.Equals(second).ToString();
+        }
+
+        public IEnumerable<TestCaseData> TestCasesForEquals
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, 0, 0, 0, 4, 5)).Returns("False");
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, 1, 2, 3)).Returns("True");
+                yield return new TestCaseData(new Polynom(0, 0), new Polynom(0, 0, 0, 0, 0)).Returns("False");
+                yield return new TestCaseData(new Polynom(0, 0, 0), new Polynom(0, 0, 0)).Returns("True");
+                yield return new TestCaseData(new Polynom(1, 2, 3), null).Returns("False");
+            }
+        }
+        #endregion
+
+        #region Test method:  public override int GetHashCode()
+        [Test, TestCaseSource(nameof(TestCasesForGetHashCode))]
+        public string ComparingTwoHashesOfPolynoms_ToString(Polynom first, Polynom second)
+        {
+            return (first.GetHashCode() == second.GetHashCode()).ToString();
+        }
+
+        public IEnumerable<TestCaseData> TestCasesForGetHashCode
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, 0, 0, 0, 4, 5)).Returns("False");
+                yield return new TestCaseData(new Polynom(0, 1, 2, 3), new Polynom(0, 1, 2, 3)).Returns("True");
+                yield return new TestCaseData(new Polynom(0, 0), new Polynom(0, 0, 0, 0, 0)).Returns("False");
+                yield return new TestCaseData(new Polynom(0, 0, 0), new Polynom(0, 0, 0)).Returns("True");
+            }
+        }
+        #endregion
+
+    }
+}
